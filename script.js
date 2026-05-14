b5d838477a0a403f2907560fd378ccbfd0205994
Tharnish palanisamy <tharnishpalanisamy3@gmail.com>
2026-05-14 17:24:30 +0530

    final cleanup

--- script.js
+++ script.js
@@ -1,44 +1,28 @@
-
-
 const carousel = document.getElementById("watchCarousel");
-
 document.querySelector(".right-btn").addEventListener("click", () => {
-
     carousel.scrollBy({
         left: 300,
         behavior: "smooth"
     });
-
 });
-
 document.querySelector(".left-btn").addEventListener("click", () => {
-
     carousel.scrollBy({
         left: -300,
         behavior: "smooth"
     });
-
 });
-
 const secondCarousel = document.getElementById("secondWatchCarousel");
-
 document.querySelector(".second-right-btn")
 .addEventListener("click", () => {
-
     secondCarousel.scrollBy({
         left: 300,
         behavior: "smooth"
     });
-
 });
-
 document.querySelector(".second-left-btn")
 .addEventListener("click", () => {
-
     secondCarousel.scrollBy({
         left: -300,
         behavior: "smooth"
     });
-
 });
-
